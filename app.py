045a5a397d0b4e855f57aeef9280b68c391c6465
ashu23hgs-png <ashu.23.hgs@gmail.com>
2026-06-14 20:20:37 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -102,9 +102,15 @@ def things_to_do_in_bangalore():
 @app.route('/best-stargazing-places-near-bangalore-2026.html')
 def best_stargazing_places_near_bangalore():
     return send_from_directory(".", "best-stargazing-places-near-bangalore-2026.html")
-    @app.rout('/pintrest-e90f8.html')
+    @app.route('/pintrest-e90f8.html')
     def pin():
         return send_from_directory(".","pintrest-e90f8.html")
+          @app.route('/noctourism-india.html')
+    def pin():
+        return send_from_directory(".","noctourism-india.html")
+        @app.route('/meteor-shower-calendar-india-2027.html')
+    def pin():
+        return send_from_directory(".","meteor-shower-calendar-india-2027.html")
 GENERIC_PLACE_PATTERNS = [
     re.compile(
         r"\b(old town walk|public square|street food lane|city center|top attraction|"
