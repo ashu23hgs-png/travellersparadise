1597c3712260be4281a79bae42377c564cf06d65
ashu23hgs-png <ashu.23.hgs@gmail.com>
2026-06-14 21:59:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,16 +103,16 @@ def things_to_do_in_bangalore():
 def best_stargazing_places_near_bangalore():
     return send_from_directory(".", "best-stargazing-places-near-bangalore-2026-guide.html")
 @app.route('/pintrest-e90f8.html')
-    def pin():
-        return send_from_directory(".","pintrest-e90f8.html")
+def pin():
+    return send_from_directory(".","pintrest-e90f8.html")
           
- @app.route('/noctourism-india.html')
-    def noc():
-        return send_from_directory(".","noctourism-india.html")
+@app.route('/noctourism-india.html')
+def noc():
+    return send_from_directory(".","noctourism-india.html")
        
- @app.route('/meteor-shower-calendar-india-2027.html')
-    def mete():
-        return send_from_directory(".","meteor-shower-calendar-india-2027.html")
+@app.route('/meteor-shower-calendar-india-2027.html')
+def mete():
+    return send_from_directory(".","meteor-shower-calendar-india-2027.html")
 GENERIC_PLACE_PATTERNS = [
     re.compile(
         r"\b(old town walk|public square|street food lane|city center|top attraction|"
